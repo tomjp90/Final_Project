c2565d1170633eaaa510a7a1d588503dc706ed9e
Tom Peddlesden <tpeddlesden@gmail.com>
2021-05-22 12:55:19 +1000

    ready for heroku, modified scrape

--- scrape.py
+++ scrape.py
@@ -44,83 +44,83 @@ def scrape_house_listing(url):
             driver.get(url)            
             html = driver.page_source 
       
-            driver.quit()
+            
         
 
+
+            # find and store relevant html
+            soup = bs(html, 'html.parser')
+            summary = soup.find("div", class_="css-fpm9y")
+            property_features = summary.find_all('span', class_='css-1rzse3v')
+            
+            # extract property features
+            bedrooms = property_features[0].text.split(' ')[0]
+            bathrooms = property_features[1].text.split(' ')[0]  
+
+            # try and catch when features cannot be scraped
             try:
-                  # find and store relevant html
-                  soup = bs(html, 'html.parser')
-                  summary = soup.find("div", class_="css-fpm9y")
-                  property_features = summary.find_all('span', class_='css-1rzse3v')
-                  
-                  # extract property features
-                  bedrooms = property_features[0].text.split(' ')[0]
-                  bathrooms = property_features[1].text.split(' ')[0]  
+                  cars = property_features[2].text.split(' ')[0] 
+            except:
+                  cars = "Unknown"
+            try:
+                  landsize = property_features[3].text.split(' ')[0][:-2]
+            except:
+                  landsize = "Unknown"                  
+            try:
+                  property_type = summary.find('span', class_='css-in3yi3').text
+            except:
+                  property_type = "Unknown" 
 
-                  # try and catch when features cannot be scraped
-                  try:
-                        cars = property_features[2].text.split(' ')[0] 
-                  except:
-                        cars = "Unknown"
-                  try:
-                        landsize = property_features[3].text.split(' ')[0][:-2]
-                  except:
-                        landsize = "Unknown"                  
-                  try:
-                        property_type = summary.find('span', class_='css-in3yi3').text
-                  except:
-                        property_type = "Unknown" 
+            # find address from property features
+            address = summary.find('h1', class_='css-164r41r').text
+            postcode = address.split(' ')[-1]
+            state = address.split(' ')[-2]       
 
-                  # find address from property features
-                  address = summary.find('h1', class_='css-164r41r').text
-                  postcode = address.split(' ')[-1]
-                  state = address.split(' ')[-2]       
+            # split address around suburb to extract one or two worded suburbs
+            street_types = ["street","Street","avenue","Avenue","road","Road","st","St","Rd"]
+            state_names = ["VIC", "NSW", "QLD", "SA", "WA", "NT", "TAS", "ACT"]
+            for t in street_types:
+                  if t in address:
+                        temp = address.split(f"{t} ")[1]                                
+                        for state in state_names:
+                              if state in temp:
+                                    suburb = temp.split(f" {state}")[0]                                              
+                                    break
+                        break
 
-                  # split address around suburb to extract one or two worded suburbs
-                  street_types = ["street","Street","avenue","Avenue","road","Road","st","St","Rd"]
-                  state_names = ["VIC", "NSW", "QLD", "SA", "WA", "NT", "TAS", "ACT"]
-                  for t in street_types:
-                        if t in address:
-                              temp = address.split(f"{t} ")[1]                                
-                              for state in state_names:
-                                    if state in temp:
-                                          suburb = temp.split(f" {state}")[0]                                              
-                                          break
-                              break
-
-                  # find property feature image
-                  property_img = soup.find("picture", class_="css-8yo374").find("source")['srcset']            
-                  
-                  # pass all features to dictionary 
-                  house_features = {
-                        "listing_url": url,
-                        "bedrooms": bedrooms,
-                        "bathrooms": bathrooms,
-                        "cars": cars,
-                        "landsize": landsize,
-                        "image_url": property_img,
-                        "address": address,
-                        "ptype": property_type,
-                        "postcode": postcode,
-                        "state": state,
-                        "suburb": suburb,
-                        "day": current_day,
-                        "month": current_month,
-                        "year": current_year,
-                        "prediction": [],
-                        "melbourne_avg": [],
-                        "suburb_distance_crime": [],
-                        "predict_format": [],
-                        "future_predict": []                        
-                        }
+            # find property feature image
+            property_img = soup.find("picture", class_="css-8yo374").find("source")['srcset']            
+            
+            # pass all features to dictionary 
+            house_features = {
+                  "listing_url": url,
+                  "bedrooms": bedrooms,
+                  "bathrooms": bathrooms,
+                  "cars": cars,
+                  "landsize": landsize,
+                  "image_url": property_img,
+                  "address": address,
+                  "ptype": property_type,
+                  "postcode": postcode,
+                  "state": state,
+                  "suburb": suburb,
+                  "day": current_day,
+                  "month": current_month,
+                  "year": current_year,
+                  "prediction": [],
+                  "melbourne_avg": [],
+                  "suburb_distance_crime": [],
+                  "predict_format": [],
+                  "future_predict": []                        
+                  }
 
       # results if scraping failed
-            except:
+            
 
-                  house_features = "error"        
+                  # house_features = "error"        
       
       except:
-            browser.quit()
+            driver.quit()
             house_features = "Invalid URL"
 
       # return dictionary of features
