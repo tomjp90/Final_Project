a68528c7d5ea1813f43a8d85b3bd68cfb87636e7
Tom Peddlesden <72062388+tomjp90@users.noreply.github.com>
2021-06-06 19:18:00 +1000

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -36,10 +36,10 @@ def scrape_house_listing(url):
             rand = np.random.randint(6)
             headers_list = [{'User-Agent': 'Chrome/6.1 (Mac; Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102',},
                               {'User-Agent': 'Opera/8.4 (Macintosh; Mac OS X 10_11_6) Kit/538.36 (KHTML, like Gecko) Opera/50.1.3.54'},
-                              {'User-Agent': 'Chrome/5.1 (Windows; Win OS 10.3) WebKit/537.36 (HTML) Chrome/50.0.21.12'},
-                              {'User-Agent': 'Chrome/5.2 (Windows; Win OS 10.3) WebKit/537.36 (KHTML, like Gecko) Chrome/5012.0.21.12'},
-                              {'User-Agent': 'Chrome/5.1 (Windows; Win OS 10.3) WebKit/537.36 (like Gecko) Chrome/53.0.21.12'},
-                              {'User-Agent': 'Chrome/5.1 (Macintosh; Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102'}
+                              {'User-Agent': 'Chrome/5.11 (Win; Win OS 10.3) WebKit/537.36 (HTML) Chrome/50.0.21.12'},
+                              {'User-Agent': 'Chrome/5.22 (Win; Win OS 10.3) WebKit/537.36 (KHTML, like Gecko) Chrome/5012.0.21.12'},
+                              {'User-Agent': 'Chrome/5.11 (Win; Win OS 10.3) WebKit/537.36 (like Gecko) Chrome/53.0.21.12'},
+                              {'User-Agent': 'Chrome/5.11 (Mac; Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102'}
                               ]      
                                   
             headers = headers_list[rand]
